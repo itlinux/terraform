32a36fe1f0cc2b351ae6b207c4f066047e823c00
Yantra Kalpana <yantra.kalpana@gmail.com>
2020-04-28 18:04:49 -0500

    terraform examples

--- p4/dynamic/var_gcp.tf
+++ p4/dynamic/var_gcp.tf
@@ -1,7 +1,7 @@
 variable "gcp-terraform" {
     type = map(string)
     default = {
-        sa_json = "/Users/work/Documents/TERRAFORM/.secrets/terraform_sa.json"
-        project = "terraform-demo-2020-04-26"
+        sa_json = "CHANGE_IT_service_account.json"
+        project = "terraform-demo"
     }
 }
